d6d022b99efc0336a97b00fa4a06354f9683f730
KevinKaunan <kleintomaskevin7@gmail.com>
2020-05-18 15:29:54 +0200

    Si

--- app/src/main/java/com/example/primersprint/MenuActivity.java
+++ app/src/main/java/com/example/primersprint/MenuActivity.java
@@ -4,6 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -15,7 +21,9 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class MenuActivity extends AppCompatActivity {
-
+    GridView gridView;
+    String[] fruitNames = {"Apple","Orange","strawberry","Melon","Kiwi","Banana"};
+    int[] fruitImages = {R.drawable.apple,R.drawable.oranges,R.drawable.strawberry,R.drawable.watermelon,R.drawable.kiwi,R.drawable.banana};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +45,54 @@ public class MenuActivity extends AppCompatActivity {
             return false;
         });
 
+        gridView = findViewById(R.id.gridview);
+
+        CustomAdapter customAdapter = new CustomAdapter();
+        gridView.setAdapter(customAdapter);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), GridItemActivity.class);
+                intent.putExtra("name", fruitNames[position]);
+                intent.putExtra("imnage", fruitImages[position]);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+
+    private class CustomAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return fruitImages.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view1 = getLayoutInflater().inflate(R.layout.row_data, null);
+
+            TextView name= view1.findViewById(R.id.fruits);
+            ImageView iamge = view1.findViewById(R.id.images);
+
+            name.setText(fruitNames[position]);
+            iamge.setImageResource(fruitImages[position]);
+
+
+            return view1;
+        }
     }
 //
 
