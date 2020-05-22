43d5f2e289d0b5b8f1e2dc6dadf4da87139d9626
AQUIL3S <andreshermosodemendoza@gmail.com>
2020-05-22 13:03:44 +0200

    GeoPics 1.0 Update con searchview y camara

--- app/src/main/java/com/example/primersprint/ui/Perfil/PerfilFragment.java
+++ app/src/main/java/com/example/primersprint/ui/Perfil/PerfilFragment.java
@@ -14,12 +14,12 @@ import com.example.primersprint.R;
 public class PerfilFragment extends Fragment {
 
     private PerfilViewModel PerfilViewModel;
-
+//creo q esto se puede eliminar cambio de perfil a login
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         PerfilViewModel =
                 ViewModelProviders.of(this).get(PerfilViewModel.class);
-        View root = inflater.inflate(R.layout.perfil, container, false);
+        View root = inflater.inflate(R.layout.activity_login, container, false);
 
         return root;
     }
